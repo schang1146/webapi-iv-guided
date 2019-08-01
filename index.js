25d4df554d8934ca017dbf1132f48f73ea0b9577
schang1146 <sammychang2185@gmail.com>
2019-07-31 19:22:35 -0700

    added root

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const server = require('./api/server.js');
-require('dotenv').config();
+const server = require("./api/server.js");
+require("dotenv").config();
 
 const port = process.env.PORT ? process.env.PORT : 4000;
 
+server.get("/", (req, res) => {
+    res.send(200).json({});
+});
+
 server.listen(4000, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
