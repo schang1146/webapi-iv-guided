22ba331e59eeb782304bf1d8746ebb5c1efbd7ac
schang1146 <sammychang2185@gmail.com>
2019-07-31 19:27:13 -0700

    removed root '/'

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ require("dotenv").config();
 
 const port = process.env.PORT ? process.env.PORT : 4000;
 
-server.get("/", (req, res) => {
-    res.send(200).json({});
-});
-
 server.listen(4000, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
